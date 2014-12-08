b8467c4b0b65a5c16691ad1510966412f9934be7
Paul <crazy046@hotmail.co.uk>
2014-12-08 19:32:13 +0000

    section recovery

--- src/Faversham_hall/config.c
+++ src/Faversham_hall/config.c
@@ -1,6 +1,8 @@
 #include "config.h"
 #include "../SSL/SSL.h"
 
+static int SECTION_COUNT = 2;
+
 static char *title_default = "this is the window title";
 static int window_width_default = 800;
 static int window_height_default = 600;
@@ -60,6 +62,22 @@ static int create_config() {
 	return 0;
 }
 
+static void check_sections_exit(SSL_IniFile *ini) {
+	int i;
+	for (i = 0; i < SECTION_COUNT; i++) {
+		if(!SSL_Inifile_GetSection_Name(ini, i)) {
+			FILE *file = fopen(CONFIG_PATH, "a");
+			fprintf(file,"\n");
+			if (i == 0) {
+				print_display_block(file);
+			} else if (i == 1) {
+				print_game_block(file);
+			}
+			fclose(file);
+		}
+	}
+}
+
 int load_config(char *path) {
 	SSL_IniFile *ini = SSL_IniFIle_Create();		// create the ini reader
 
@@ -76,19 +94,26 @@ int load_config(char *path) {
 	if (ini->dictionary == NULL) {					// make sure it was loaded
 		return -1;
 	}
+
+	check_sections_exit(ini);
 													// read in the information
-	WINDOW_TITLE = SSL_IniFile_GetString(ini, "display", "title", NULL);
+													// if a key is missing use default and print error to log.
+	WINDOW_TITLE = SSL_IniFile_GetString(ini, "display", "title", (void *)-1);
+	if (WINDOW_TITLE == (void *)-1) {
+		WINDOW_TITLE = "this is the window title";
+		SSL_Log_Write("Error: ini missing window title, reverting to default!");
+	}
 
-	WINDOW_WIDTH = SSL_IniFile_GetInt(ini, "display", "window_width", (int)NULL);
-	WINDOW_HEIGHT = SSL_IniFile_GetInt(ini, "display", "window_height", (int)NULL);
-	WINDOW_RES_WIDTH = SSL_IniFile_GetInt(ini, "display", "window_res_width", (int)NULL);
-	WINDOW_RES_HEIGHT = SSL_IniFile_GetInt(ini, "display", "window_res_height", (int)NULL);
+	WINDOW_WIDTH = SSL_IniFile_GetInt(ini, "display", "window_width", (int)-1);
+	WINDOW_HEIGHT = SSL_IniFile_GetInt(ini, "display", "window_height", (int)-1);
+	WINDOW_RES_WIDTH = SSL_IniFile_GetInt(ini, "display", "window_res_width", (int)-1);
+	WINDOW_RES_HEIGHT = SSL_IniFile_GetInt(ini, "display", "window_res_height", (int)-1);
 
-	MAX_TICKS_PER_SECOND = SSL_IniFile_GetInt(ini, "game", "max_ticks_per_second", (int)NULL);
-	TEXTURE_SCALING = SSL_IniFile_GetInt(ini, "game", "smooth_texture_scaling", (int)NULL);
-	VSYNC = SSL_IniFile_GetInt(ini, "game", "vsync", (int)NULL);
-	WINDOWED = SSL_IniFile_GetInt(ini, "game", "windowed", (int)NULL);
-	BORDERLESS = SSL_IniFile_GetInt(ini, "game", "borderless", (int)NULL);
+	MAX_TICKS_PER_SECOND = SSL_IniFile_GetInt(ini, "game", "max_ticks_per_second", (int)-1);
+	TEXTURE_SCALING = SSL_IniFile_GetInt(ini, "game", "smooth_texture_scaling", (int)-1);
+	VSYNC = SSL_IniFile_GetInt(ini, "game", "vsync", (int)-1);
+	WINDOWED = SSL_IniFile_GetInt(ini, "game", "windowed", (int)-1);
+	BORDERLESS = SSL_IniFile_GetInt(ini, "game", "borderless", (int)-1);
 
 	return 0;
 }
