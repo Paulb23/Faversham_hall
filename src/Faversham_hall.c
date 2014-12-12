8cb786f7055c06a8543abe855deda1ed50e496c8
Paul <crazy046@hotmail.co.uk>
2014-12-12 13:03:41 +0000

    libary init

--- src/Faversham_hall.c
+++ src/Faversham_hall.c
@@ -1,8 +1,20 @@
 #include "Faversham_hall/config.h"
 #include "Faversham_hall/window_manager.h"
 #include "Faversham_hall/game/game.h"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_Mixer.h"
+#include "SDL2/SDL_Image.h"
+#include "SDL2/SDL_TTF.h"
+#include "SSL/SSL.h"
 
 int main(int argc, char *argv[]) {
+
+	// init libaries
+	SDL_Init(SDL_INIT_EVERYTHING);
+	IMG_Init(IMG_INIT_PNG);
+	Mix_Init(MIX_INIT_MP3);
+	Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 4096);
+	SSL_Init();
 
 	load_config("../conf/config.ini");
 	create_window();
