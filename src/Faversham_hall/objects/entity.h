b6e2d281ee170c198d72fd42da0f23e203c39517
Paul <crazy046@hotmail.co.uk>
2014-12-14 13:39:36 +0000

    entity destoy + docs

--- src/Faversham_hall/objects/entity.h
+++ src/Faversham_hall/objects/entity.h
@@ -1,26 +1,74 @@
+/*-------------------------------------------------------------------------*/
+/**
+   @file    entity.h
+   @author  P. Batty
+   @brief   Implements a the entity system for the game
+
+   This module implements the entity system for the game, and sprite
+   system.
+*/
+/*--------------------------------------------------------------------------*/
+
 #ifndef ENTITY_H_
 #define ENTITY_H_
 
+
+/*---------------------------------------------------------------------------
+                                Includes
+ ---------------------------------------------------------------------------*/
+
 #include "../../SSL/SSL.h"
 
+
+/*---------------------------------------------------------------------------
+                                New types
+ ---------------------------------------------------------------------------*/
+
+/*!--------------------------------------------------------------------------
+  @brief    Entity_Pos
+
+  This object contains information the entities position
+
+\----------------------------------------------------------------------------*/
 typedef struct _entity_pos_ {
-	int x;
-	int y;
-	int w;
-	int h;
-} entity_pos;
+	int x;						/**< X position of the entity */
+	int y;						/**< Y position of the entity */
+	int w;						/**< Width of the entity */
+	int h;						/**< Height of the entity */
+} Entity_pos;
 
+
+/*!--------------------------------------------------------------------------
+  @brief    Entity_image
+
+  This object contains information the entities image
+
+\----------------------------------------------------------------------------*/
 typedef struct _entity_image_ {
-	SSL_Image *image;
-	int max_frames;
-	int current_frame;
-	long time_frame;
-	long next_frame;
-} entity_image;
+	SSL_Image *image;			/**< image of the entity */
+	int max_frames;				/**< max frame per animation */
+	int current_frame;			/**< current frame of the animation */
+	long time_frame;			/**< time between frames */
+	long next_frame;			/**< time to change frames */
+} Entity_image;
 
+
+/*!--------------------------------------------------------------------------
+  @brief    Entity
+
+  This object contains information about all the entity
+
+\----------------------------------------------------------------------------*/
 typedef struct _entity_ {
-	entity_image image;
-	entity_pos pos;
-} entity;
+	Entity_image image;			/**< image information of the entity */
+	Entity_pos pos;				/**< position of the entity */
+} Entity;
+
+
+/*---------------------------------------------------------------------------
+                            Function prototypes
+ ---------------------------------------------------------------------------*/
+
+void entity_destroy(Entity *entity);
 
 #endif
