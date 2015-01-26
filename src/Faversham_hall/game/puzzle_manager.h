f0ca9a4eae1e81901cd23b147d64650356363553
Paul <crazy046@hotmail.co.uk>
2015-01-26 22:04:35 +0000

    puzzle manager docs

--- src/Faversham_hall/game/puzzle_manager.h
+++ src/Faversham_hall/game/puzzle_manager.h
@@ -27,7 +27,27 @@
                             Function prototypes
  ---------------------------------------------------------------------------*/
 
+/*!--------------------------------------------------------------------------
+  @brief    Validates the clue
+  @param	act			act number to check on
+  @param	mission		mission number to check on
+  @return 1 if valued else 0
+
+  Checks if the clue can be access at the current point. return1 if valid else 0
+
+\-----------------------------------------------------------------------------*/
 int valid_clue(int act, int mission);
+
+
+/*!--------------------------------------------------------------------------
+  @brief    Starts the clue
+  @param	act			act number to check on
+  @param	mission		mission number to check on
+  @return Void
+
+  Starts the clue / puzzle and passes it on to the mission manager
+
+\-----------------------------------------------------------------------------*/
 void start_clue(int act, int mission);
 
 #endif
