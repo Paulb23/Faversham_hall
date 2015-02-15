083d286f0b76c154703411f97562585e2ee82720
Paul <crazy046@hotmail.co.uk>
2015-02-15 18:51:24 +0000

    dialog comments + font colour change

--- src/Faversham_hall/game/dialogue_parser.c
+++ src/Faversham_hall/game/dialogue_parser.c
@@ -24,51 +24,66 @@
                             Private functions
  ---------------------------------------------------------------------------*/
 
-static int max_chars = 41;
-static int max_lines_per_page = 5;
+static int max_chars = 41;				/**< max number of chars per line */
+static int max_lines_per_page = 5;		/**< max number of lines per page */
 
-static int max_option_chars = 11;
+static int max_option_chars = 11;		/**< max number of option chars per line */
 
-static char *dialog_path = "../extras/resources/dialog/";
-static SSL_IniFile *dialog;
+static char *dialog_path = "../extras/resources/dialog/";	/**< path to the dialog files */
+static SSL_IniFile *dialog;									/**< the ini file we a re currently reading */
 
-static char *name;
-static char *text;
-static int number_of_lines;
-static int number_of_pages;
-static int current_line;
-static int current_page_number;
-static char *current_node;
-static int option_count;
-static SSL_List *options;
+static char *name;				/**< name of the npc currently talking */
+static char *text;				/**< text that the npc is saying */
+static int number_of_lines;		/**< number of lines in that text */
+static int number_of_pages;		/**< number of pages in that text */
+static int current_line;		/**< current line  number we a re on */
+static int current_page_number;	/**< current page we are on */
+static char *current_node;		/**< current dialog node we are reading */
+static int option_count;		/**< number of options that we have */
+static SSL_List *options;		/**< list of the options */
 
-static SSL_Image *dialog_back;
-static SSL_Image *portait;
+static SSL_Image *dialog_back;	/**< the dialog background */
+static SSL_Image *portait;		/**< the portait of the character */
+
+
+/*----------------------------------
+     loads a dialog node
+ ----------------------------------*/
 
 static void load_node(char *node) {
-	max_chars = 41;
+
+	// set the current node to this one
 	current_node = node;
+
+	// read in the name and text
 	name = SSL_IniFile_GetString(dialog, node, "name", "detective");
 	text = SSL_IniFile_GetString(dialog, node, "text", "detective");
 
+	// swap in case of called by different name
 	if (strcmp(name, "assistant") == 0) {
 		name = "companion";
 	}
 
+	// calculate the number of lines
 	number_of_lines = ((strlen(text) - 1)/ max_chars);
 
+	// calculate the number of pages
 	if (number_of_lines != 0) {
 		number_of_pages = (number_of_lines / max_lines_per_page);
 	} else {
 		number_of_pages = 0;
 	}
 
+	// set to page and line 0
 	current_line = 0;
 	current_page_number = 0;
 
+	// get the numbner of options
 	option_count = SSL_IniFile_GetInt(dialog, node, "option_count", 0);
+
+	// read the options if we have some
 	if (option_count != 0) {
-		options = SSL_List_Create(); // memory leak todo: fix :D
+		options = SSL_List_Create();
 
 		int i;
 		for (i = 1; i <= option_count; i++) {
@@ -78,9 +93,12 @@ static void load_node(char *node) {
 		}
 	}
 
+	// free up old portrait memory
 	if (portait) {
 		SSL_Image_Destroy(portait);
 	}
+
+	// load in the new portrait
 	char buf[100];
 	sprintf(buf, "../extras/resources/portraits/%s.png" ,name);
 	portait = SSL_Image_Load(buf, 100, 140, game_window);
@@ -90,42 +108,94 @@ static void load_node(char *node) {
                             Function codes
  ---------------------------------------------------------------------------*/
 
+/*!--------------------------------------------------------------------------
+  @brief	Inits the dialog parser
+  @return 	Void
+
+  Inits the dialog parser.
+
+\-----------------------------------------------------------------------------*/
 void dialog_init() {
 	dialog_back = SSL_Image_Load("../extras/resources/gui/dialog/dialog.png", WINDOW_RES_WIDTH, WINDOW_RES_HEIGHT, game_window);
 }
 
+
+/*!--------------------------------------------------------------------------
+  @brief	Cleans up the dialog parser
+  @return 	Void
+
+  Cleans up the dialog parser.
+
+\-----------------------------------------------------------------------------*/
 void dialog_clean_up() {
 	SSL_Image_Destroy(dialog_back);
 }
 
+
+/*!--------------------------------------------------------------------------
+  @brief	Starts the dialog
+  @param	other		The npc name
+  @param	act			The act number
+  @return 	Void
+
+  Cleans up the dialog parser.
+
+\-----------------------------------------------------------------------------*/
 void start_dialog(char *other, int act) {
+
+	// if we've previous been in dialog free up memory
 	if (dialog) {
 		SSL_IniFile_Destroy(dialog);
 	}
+
+	// load the dialog
 	dialog = SSL_IniFIle_Create();
 	char path[100];
 	sprintf(path, "%s%s%i.ini", dialog_path, other, act);
+
+	// if the file for the npc + act exists load that one
 	if( access( path, F_OK ) != -1 ) {
 		SSL_IniFile_Load(dialog, path);
 		load_node("root");
 	} else {
+
+		// else check for just the npc name
 		sprintf(path, "%s%s.ini", dialog_path, other);
 		if( access( path, F_OK ) != -1 ) {
 		} else {
 			sprintf(path, "%s%s.ini", dialog_path, "test_dialogue");
 		}
+
+		// load in the fil
 		SSL_IniFile_Load(dialog, path);
+
+		// load the root node
 		load_node("root");
 	}
 }
 
+
+/*!--------------------------------------------------------------------------
+  @brief	Dialog event handler
+  @param	event		The event queue
+  @return 	Void
+
+  Handles events for the dialog parser.
+
+\-----------------------------------------------------------------------------*/
 int update_dialog(SDL_Event event) {
+
+	// if we are not on the last page allow defualt to "press one for next page"
 	if (current_page_number < number_of_pages && number_of_lines > 1 && current_line + max_lines_per_page < number_of_lines) {
 		if (SSL_Keybord_Keyname_Pressed("_1", event)) {
 			current_line += max_lines_per_page;
 			current_page_number++;
 		}
+
+	// else we are on the last page
 	} else {
+
+		// if we don't have options, progress or exit convo based on action
 		if (option_count == 0) {
 			if (SSL_Keybord_Keyname_Pressed("_1", event)) {
 				char *action = SSL_IniFile_GetString(dialog, current_node, "action", "end");
@@ -135,6 +205,8 @@ int update_dialog(SDL_Event event) {
 					load_node(action);
 				}
 			}
+
+		// else we have options, load in when the 1 - 9 button is pressed and perform tehe corrosponding action
 		} else {
 
 			char *action = "null";
@@ -167,6 +239,7 @@ int update_dialog(SDL_Event event) {
 				action = SSL_IniFile_GetString(dialog, current_node, "option_9_action", "null");
 			}
 
+			// if null do nothing else perform the action
 			if (strcmp(action, "null") == 0) {
 
 			} else if (strcmp(action, "end") == 0) {
@@ -177,20 +250,35 @@ int update_dialog(SDL_Event event) {
 		}
 	}
 
+	// return 1 by default
 	return 1;
 }
 
+
+/*!--------------------------------------------------------------------------
+  @brief	Draws the dialog
+  @return 	Void
+
+  Draws the dialog
+
+\-----------------------------------------------------------------------------*/
 void render_dialog() {
 
+	// draw the background images
 	SSL_Image_Draw(portait, 1,2, 0,0, SDL_FLIP_NONE, game_window);
 	SSL_Image_Draw(dialog_back, 0,0, 0,0, SDL_FLIP_NONE, game_window);
 
-	int i;
-	int start_y = 150;
-	int y_inc = 15;
+	// variables for the text drawing
+	int i; // loop for lines
+	int start_y = 150; // start y
+	int y_inc = 15; // y incremental value
+
+	// if we only have one line draw it
 	if (number_of_lines == 0) {
-		SSL_Font_Draw(10, start_y, 0 ,SDL_FLIP_NONE, text, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(255,255,255,0), game_window);
+		SSL_Font_Draw(10, start_y, 0 ,SDL_FLIP_NONE, text, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(0,0,0,255), game_window);
 	} else {
+
+		// else loop though the lines and draw them
 		for (i = 0; i < max_lines_per_page; i++) {
 			if (current_line + i > number_of_lines) {
 				break;
@@ -202,33 +290,46 @@ void render_dialog() {
 			if (final_text[0] == ' ') {
 				final_text++;
 			}
-			SSL_Font_Draw(10, start_y, 0 ,SDL_FLIP_NONE, final_text, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(255,255,255,0), game_window);
+			SSL_Font_Draw(10, start_y, 0 ,SDL_FLIP_NONE, final_text, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(0,0,0,255), game_window);
 			start_y += y_inc;
 		}
 	}
+
+	// if we have no options or there are more pages, draw the default "1..."
 	if (option_count == 0 || current_page_number < number_of_pages) {
-		SSL_Font_Draw(225, 10, 0 ,SDL_FLIP_NONE, "1. ...", (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(255,255,255,0), game_window);
+		SSL_Font_Draw(225, 14, 0 ,SDL_FLIP_NONE, "1. ...", (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(0,0,0,255), game_window);
+
+	// else draw the other options
 	} else {
 		int i;
-		int y_inc = 10;
+		int y_inc = 14;
 
+		// loop through the options and eaither draw one or two lines
 		for (i = 0; i < option_count; i++) {
 			char buf[100];
 			if (strlen(SSL_List_Get_String(options, i)) < max_option_chars) {
 				sprintf(buf, "%i. %s", i+1, SSL_String_Substring(SSL_List_Get_String(options, i), 0, strlen(SSL_List_Get_String(options, i))));
-				SSL_Font_Draw(225, y_inc, 0 ,SDL_FLIP_NONE, buf, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(255,255,255,0), game_window);
+				SSL_Font_Draw(225, y_inc, 0 ,SDL_FLIP_NONE, buf, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(0,0,0,255), game_window);
 			} else {
 				sprintf(buf, "%i. %s", i+1, SSL_String_Substring(SSL_List_Get_String(options, i), 0, max_option_chars - 1));
-				SSL_Font_Draw(225, y_inc, 0 ,SDL_FLIP_NONE, buf, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(255,255,255,0), game_window);
+				SSL_Font_Draw(225, y_inc, 0 ,SDL_FLIP_NONE, buf, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(0,0,0,255), game_window);
 				y_inc+=18;
 				sprintf(buf, "%s", SSL_String_Substring(SSL_List_Get_String(options, i), max_option_chars - 1, strlen(SSL_List_Get_String(options, i))));
-				SSL_Font_Draw(225, y_inc, 0 ,SDL_FLIP_NONE, buf, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(255,255,255,0), game_window);
+				SSL_Font_Draw(225, y_inc, 0 ,SDL_FLIP_NONE, buf, (SSL_Font *)asset_manager_getFont("dialog_font"), SSL_Color_Create(0,0,0,255), game_window);
 			}
 		y_inc += 25;
 		}
 	}
 }
 
+
+/*!--------------------------------------------------------------------------
+  @brief	Gets the current dialog node
+  @return 	name of the current dialog node
+
+  Gets the current dialog node
+
+\-----------------------------------------------------------------------------*/
 char *get_node_name() {
 	return current_node;
 }
--- src/Faversham_hall/game/dialogue_parser.h
+++ src/Faversham_hall/game/dialogue_parser.h
@@ -28,11 +28,66 @@
                             Function prototypes
  ---------------------------------------------------------------------------*/
 
+/*!--------------------------------------------------------------------------
+  @brief	Inits the dialog parser
+  @return 	Void
+
+  Inits the dialog parser.
+
+\-----------------------------------------------------------------------------*/
 void dialog_init();
+
+
+/*!--------------------------------------------------------------------------
+  @brief	Cleans up the dialog parser
+  @return 	Void
+
+  Cleans up the dialog parser.
+
+\-----------------------------------------------------------------------------*/
 void dialog_clean_up();
+
+
+/*!--------------------------------------------------------------------------
+  @brief	Starts the dialog
+  @param	other		The npc name
+  @param	act			The act number
+  @return 	Void
+
+  Cleans up the dialog parser.
+
+\-----------------------------------------------------------------------------*/
 void start_dialog(char *other, int act);
+
+
+/*!--------------------------------------------------------------------------
+  @brief	Dialog event handler
+  @param	event		The event queue
+  @return 	1 if the Dialog is ended else 0
+
+  Handles events for the dialog parser. returns 1 if the Dialog is ended else 0
+
+\-----------------------------------------------------------------------------*/
 int update_dialog(SDL_Event event);
+
+
+/*!--------------------------------------------------------------------------
+  @brief	Draws the dialog
+  @return 	Void
+
+  Draws the dialog
+
+\-----------------------------------------------------------------------------*/
 void render_dialog();
+
+
+/*!--------------------------------------------------------------------------
+  @brief	Gets the current dialog node
+  @return 	name of the current dialog node
+
+  Gets the current dialog node
+
+\-----------------------------------------------------------------------------*/
 char *get_node_name();
 
 #endif
